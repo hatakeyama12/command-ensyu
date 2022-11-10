67a41a5a587669fa6755944cbb0efd1aa2bed767
hatakeyama <m.hatakeyama.sys22@morijyobi.ac.jp>
2022-11-10 12:52:44 +0900

    BMIの計算を２人分できるように修正

--- Main.java
+++ Main.java
@@ -4,13 +4,19 @@ public class Main {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("身長(m)を入力してください");
+		System.out.print("１人目の身長(m)を入力してください");
 		double height = sc.nextDouble();
-		System.out.print("体重(kg)を入力してください");
+		System.out.print("１人目の体重(kg)を入力してください");
 		double weight = sc.nextDouble();
+		System.out.print("２人目の身長(m)を入力してください");
+		double height2 = sc.nextDouble();
+		System.out.print("２人目の体重(kg)を入力してください");
+		dpuble weight2 = sc.nextDouble();
 
 		double bmi = weight / Math.pow(height, 2);
+		double bmi2 = weight2 / Math.pow(height2, 2);
 
-		System.out.println("BMI値は"+(Math.floor(bmi*100)/100)+"です。");
+		System.out.println("１人目のBMI値は"+(Math.floor(bmi*100)/100)+"です。");
+		System.out.println("２人目のBMI値は"+(Math.floor(bmi2*100)/100)+"です。");
 	}
 }	
